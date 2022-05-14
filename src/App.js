e6ff153f92e9ca17ccf1eb871d245703cfab26f5
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-05-14 11:43:05 -0700

    Assignment # 1
    Class # 1, assignment 1.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./component/navbar";
+import Footer from "./component/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to my super store!!!</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+export default App;
